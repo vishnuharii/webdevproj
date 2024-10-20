a2c23c643bfab94e40107c46fcd08c3d1d60e4d4
vishnuharii <96003813+vishnuharii@users.noreply.github.com>
2024-10-20 13:59:38 -0400

    Week 7
    delete feature is added

---
+++ week7/reusables/connect.php
@@ -0,0 +1,11 @@
+<?php
+  $connect = mysqli_connect(
+    'localhost', 
+    'root', 
+    'root', //write your password
+    'publicschools' // write your database name
+  );
+
+  if(!$connect){
+    echo "Connection Failed: " . mysqli_connect_error();
+  }
